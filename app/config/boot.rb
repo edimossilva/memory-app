f51d98343a2d399f9a68ce48b09c570c83787243
edimo sousa <edimossilva@gmail.com>
2020-09-19 18:23:10 +0300

    use bootsnap if not ci env

--- app/config/boot.rb
+++ app/config/boot.rb
@@ -1,7 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-# require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-# Bootsnap.setup(
-#   cache_dir: '/bootsnap_cache'
-# )
+require 'bootsnap/setup' unless ENV.fetch('CI_ENV')
