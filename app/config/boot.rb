1b50f0273f10c8ccd351866a88b7557c6710e843
edimo sousa <edimossilva@gmail.com>
2020-09-19 18:37:40 +0300

    config env

--- app/config/boot.rb
+++ app/config/boot.rb
@@ -1,4 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-# require 'bootsnap/setup' unless ENV['CI_ENV']
+require 'bootsnap/setup' if ENV['CI_ENV'] != 'true'
