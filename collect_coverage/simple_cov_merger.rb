2faea1ff12122b004deed8d7f3e2f90df76d298d
edimo sousa <edimossilva@gmail.com>
2020-09-20 10:36:13 +0300

    add cov generator

---
+++ collect_coverage/simple_cov_merger.rb
@@ -0,0 +1,42 @@
+class SimpleCovMerger
+  def self.report_coverage(base_dir:, ci_project_path:, project_path:)
+    new(base_dir: base_dir, ci_project_path: ci_project_path, project_path: project_path).merge_results
+  end
+
+  attr_reader :base_dir, :ci_project_path, :project_path
+
+  def initialize(base_dir:, ci_project_path:, project_path:)
+    @base_dir = base_dir
+    @ci_project_path = ci_project_path
+    @project_path = project_path
+  end
+
+  def merge_results
+    require "simplecov"
+    require "json"
+    require "pry"
+
+    SimpleCov::filters =[]
+
+    results = resultsets.map do |file|
+      hash_result = JSON.parse(clean(File.read(file)))
+      SimpleCov::Result.from_hash(hash_result)
+    end.flatten
+
+    result = SimpleCov::ResultMerger.merge_results(*results)
+    SimpleCov::ResultMerger.store_result(result)
+  end
+
+  private
+
+  def resultsets
+    Dir["#{base_dir}/*.json"]
+  end
+
+  def clean(results)
+    # binding.pry
+    results.gsub(ci_project_path, project_path)
+  end
+end
+
+SimpleCovMerger.report_coverage(base_dir: "./input", ci_project_path: "/app/app", project_path: "./../app/app")
